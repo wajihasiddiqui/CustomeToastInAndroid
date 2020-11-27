8a4df047898b0c136e63f35e8be297b00f532dbe
wajihasiddiqui <56477014+wajihasiddiqui@users.noreply.github.com>
2020-11-27 10:34:28 +0500

    custome toast

---
+++ app/src/main/java/com/example/custometoast/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.custometoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button click;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        click = findViewById(R.id.click);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                myToast();
+            }
+        });
+
+
+    }
+
+    private void myToast() {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custome_toast,(ViewGroup)findViewById(R.id.toastlayout));
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+
+        toast.show();
+
+    }
+}
